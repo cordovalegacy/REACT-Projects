18ac29a012c4d41aab58b630600a90371d536342
Brendan Cordova <cordovalegacy19@gmail.com>
2022-07-17 01:29:17 -0700

    REACT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/todo_form';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleNewTodoSubmit = (e) => {
+    e.preventDefault();
+
+    if (newTodo.length === 0) {
+      return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      complete: false
+    };
+
+    setTodos([...todos, todoItem]);
+    setNewTodo("");
+  };
+
+  const handleTodoDelete = (delIdx) => {
+    const filteredTodos = todos.filter((todo, i) => {
+      return i !== delIdx;
+    });
+
+    setTodos(filteredTodos);
+  }
+  const handleToggleComplete = (idx) => {
+    const updatedTodos = todos.map((todo, i) => {
+      if(idx === i) {
+        todo.complete = !todo.complete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='container'>
+        <form onSubmit={(e) => { 
+          handleNewTodoSubmit(e); 
+          }}>
+          <h2>To Do List:</h2>
+          <input onChange={(e) => {
+            setNewTodo(e.target.value); 
+            }} 
+            type='text'
+            value={newTodo}
+          />
+          <div>
+            <button id='todo-add'>Add</button>
+          </div>
+        </form>
+
+            <hr />
+
+            <div id='todo-wrapper'>
+        {
+          todos.map((todo, i) => {
+            const todoClasses = ['bold', 'italic'];
+
+            if(todo.complete) {
+              todoClasses.push('line-through');
+            }
+
+            return (
+            <div id='todo-list' key={i}>
+              <input onChange={(e) => {
+                handleToggleComplete(i);
+              }} checked={todo.complete} type="checkbox" />
+              <span className={todoClasses.join(" ")}>{todo.text}</span>
+              <button id="todo-delete" onClick={(e) => {
+                handleTodoDelete(i);
+              }}>Delete</button>
+            </div>
+            );
+          })
+        }
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/todo_form.js
@@ -0,0 +1,6 @@
+import {useState} from 'react';
+
+const Form = (e) => {
+}
+
+export default Form;
